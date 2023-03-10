2e0988135f842ca38814b725d307ed2a4ac359ec
rerecum <kacperkacperkacper132@gmail.com>
2023-03-10 11:16:06 +0100

    Conditions for 2D moves like move box, move player etc.

--- src/Sokoban.js
+++ src/Sokoban.js
@@ -79,6 +79,49 @@ function getInitialState(levelNo) {
     }
 }
 
+function GameReducer(state, action) {
+    switch (action.type) {
+
+        case ACTION.RestartLevel:
+            return {...getInitialState(state.levelNo), status: gamestate.Running}
+
+        case ACTION.PlayNextLevel:
+            return {...getInitialState(state.levelNo+1), status: gamestate.Running}
+
+        case ACTION.move:
+            let d = {x: 0, y: 0} // d -> direction, b -> box
+            console.log(action.keyCode)
+            // Simple moves action
+            if (direction.Left === action.keyCode)  d.x-- 
+            if (direction.Right === action.keyCode) d.x++
+            if (direction.Up === action.keyCode)    d.y--
+            if (direction.Down === action.keyCode)  d.y++
+
+            // Check walls
+            if (state.level[state.player.y+d.y][state.player.x+d.x] === item.Wall) return {...state}
+
+            // Check if player is trying to move the box
+            if ([...state.box].find(b.x===state.player.x+d.x && b.y===state.player.y+d.y)) {
+                // Check whether it is possible to move the box
+                if (
+                    // check free space behind box
+                    [item.Playground, item.Storage].includes(state.level[state.player.y+2*d.y][state.player.x+2*d.x])
+                    // check box-free space behind box
+                    && ![...state.box].find(b => b.x === state.player.x+2*d.x && b.y === state.player.y+2*d.y) 
+                ) { // return new position with moved box
+                    let newState = {...state, player: {x: state.player.x+d.x, y: state.player.y+d.y}, box: [...state.box].map(b => {
+                        // if player tries to move the box's position
+                        if ( (b.x === state.player.x+d.x) && (b.y === state.player.y+d.y) )
+                            return {x: b.x+d.x, y: b.y+d.y}
+                        else
+                            return b
+                    } ) }
+                }
+            }
+    }
+}
+
+//<================================>
 export default function Sokoban() {
 
 }
