2fc249096658dde17df774ac3c5c1bccfb173631
rerecum <kacperkacperkacper132@gmail.com>
2023-03-10 10:32:00 +0100

    imports and DOM render in index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Sokoban from './Sokoban';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Sokoban />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
